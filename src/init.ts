7d40b972be7350d4db41caf7971a217eb8b947ad
Jadenv <jvelasco20081@gmail.com>
2026-04-03 16:05:11 -0700

    hooking?

--- src/init.ts
+++ src/init.ts
@@ -1,17 +1,39 @@
 import { env } from "node:process"
 import * as readline from "node:readline/promises"
-import { existsSync } from "node:fs"
-export function env_vars() {
+import { existsSync, writeFile, appendFile } from "node:fs"
+import { stdin as input, stdout as output } from "node:process"
+export async function env_vars(): Promise<void> {
   if (existsSync('.env')) {
 
-    console.log("does  exist")
+
 
 
   }
-  else
-    console.log("doesn't exist")
+  else {
+    const ogWrite = process.stdout.write.bind(process.stdout);
+    const rl: readline.Interface = readline.createInterface({ input, output });
+    const username: string = await rl.question("email for github :");
+    const password: Promise<string> = rl.question("password for github: ");
+    process.stdout.write = function (
+      data: string | Uint8Array,
+      encodingOrCb?: BufferEncoding | ((err?: Error | null) => void),
+      cb?: (err?: Error | null) => void
+    ): boolean {
+      const str = typeof data === "string" ? data : data.toString();
+      if (str.includes("\n")) {
+        process.stdout.write = ogWrite;
+      }
+      return ogWrite("*", encodingOrCb as BufferEncoding, cb);
+    };
+    const content = `EMAIL={}`
+    writeFile(".env", await password, (err: unknown) => {
+
+
+    });
 
 
 
+  }
 
 }
+
